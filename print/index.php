544857fce154dd3678ddd173a977ba98cbf93015
kaido.tikk <kaido.tikk@khk.ee>
2018-12-13 13:06:43 +0200

    väljastamine

---
+++ print/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaido.tikk
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+// teksti väljastamine
+// print ja echo
+print('Tere Maailm! <br>');
+echo 'PHP Alused<br>';
+print 'Nii toimub väljastamine<br>';
+// väärtuste ühendamine .
+echo 'See '.'on'.' kolmest sõnast kokku pantud<br>';
+?>
